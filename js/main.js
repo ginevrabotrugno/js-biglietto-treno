2fe13ad2a3923780be72a355b45774aa64af31d5
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-04 18:11:20 +0200

    add style

--- js/main.js
+++ js/main.js
@@ -5,10 +5,15 @@ let chilometriViaggio = parseInt(prompt ("Inserisci il numero di chilometri da p
 
 console.log("numero chilometri: ", chilometriViaggio);
 
+document.getElementById("chilometri").innerHTML = chilometriViaggio;
+
 // e l’età del passeggero.
 let etaPasseggero = parseInt(prompt ("Inserisci la tua età"));
 
 console.log("età passeggero: ", etaPasseggero);
+
+document.getElementById("eta").innerHTML = etaPasseggero;
+
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 
@@ -18,15 +23,24 @@ let prezzoBiglietto = chilometriViaggio * 0.21;
 
 console.log("prezzo biglietto: €", prezzoBiglietto.toFixed(2));
 
+document.getElementById("prezzo_intero").innerHTML = prezzoBiglietto;
+
+
 if (etaPasseggero < 18) { // va applicato uno sconto del 20% per gli under 18
 
     let sconto = (prezzoBiglietto * 0.2);
 
     console.log("sconto: -€", sconto.toFixed(2));
 
-    let prezzoFinale = prezzoBiglietto - sconto;
+    document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+
+
+    let prezzoFinale = prezzoBiglietto - sconto.toFixed(2);
 
     console.log("prezzo intero: €", prezzoFinale.toFixed(2));
+
+    document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
+
 
 } else if ( etaPasseggero > 65) { // va applicato uno sconto del 40% per gli over 65
 
@@ -34,27 +48,29 @@ if (etaPasseggero < 18) { // va applicato uno sconto del 20% per gli under 18
 
     console.log("sconto: -€", sconto.toFixed(2));
 
+    document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+
+    let prezzoFinale = prezzoBiglietto - sconto;
+
+    // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+    console.log("prezzo finale: €", prezzoFinale.toFixed(2));
+
+    document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
+
+} else {
+    let sconto = 0;
+
+    console.log("sconto: -€", sconto);
+
+    document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+
     let prezzoFinale = prezzoBiglietto - sconto;
 
     console.log("prezzo finale: €", prezzoFinale.toFixed(2));
+
+    document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
 }
 
 
 
 
-
-
-
-
-
-
-
-    
-
-    
-
-    
-
-
-
-// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
